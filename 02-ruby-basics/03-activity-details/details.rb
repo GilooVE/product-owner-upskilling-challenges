3c300908ed78f0ef9a24754cda56da6e33c8a41c
Giloo <gve@lucca.fr>
2019-10-09 15:19:32 +0000

    ruby exo 3

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,7 @@
-activity = {
-  # TODO: gather the characteristics of your preferred activity
-}
+activity = {"name" => "Visit art museum", "category" => "Cultural", "city" => "Nantes","duration" => "3 hours", "number of participants" => "5", "price (in EUR)" => "30"}
 
-# TODO: your code goes here
+puts "#{activity["name"]} [#{activity["category"].upcase}]"
+puts "Located in #{activity["city"]}"
+puts "Duration of #{activity["duration"]}"
+puts "For #{activity["number of participants"]} participants"
+puts "Charged #{activity["price (in EUR)"]}€"
