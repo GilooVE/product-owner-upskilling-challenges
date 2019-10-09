3b895cbb550256a05195607aeda2d54ecb42ef47
Giloo <gve@lucca.fr>
2019-10-09 15:40:33 +0000

    ruby exo 4

--- 02-ruby-basics/04-listing-activities/listing.rb
+++ 02-ruby-basics/04-listing-activities/listing.rb
@@ -1,5 +1,12 @@
-activities = [
-  # TODO: gather at least 3 activities
+activities = [{"name" => "Visit art museum", "category" => "Cultural", "city" => "Nantes","duration" => 120, "number of participants" => 5, "price (in EUR)" => 30},
+  {"name" => "Diner dans les arbres", "category" => "Food", "city" => "Nantes","duration" => 180, "number of participants" => 2, "price (in EUR)" => 100},
+  {"name" => "Escape Game", "category" => "Adventure", "city" => "Paris","duration" => 60, "number of participants" => 10, "price (in EUR)" => 50}
 ]
-
-# TODO: your code goes here
+activities.each do |activity|
+  puts "#{activity["name"]} [#{activity["category"].upcase}]"
+  puts "Located in #{activity["city"]}"
+  puts "Duration of #{activity["duration"]} minutes"
+  puts "For #{activity["number of participants"]} participants"
+  puts "Charged #{activity["price (in EUR)"]} €"
+  puts "---"
+end
