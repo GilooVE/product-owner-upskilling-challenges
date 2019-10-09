6e6e7baa8c7077efe3e15287718fe6a093345167
Giloo <gve@lucca.fr>
2019-10-09 14:29:08 +0000

    Ruby exo 2

--- 02-ruby-basics/02-listing-categories/categories.rb
+++ 02-ruby-basics/02-listing-categories/categories.rb
@@ -1,5 +1,11 @@
-categories = [
-  # TODO: gather the categories of activity
-]
-
 # TODO: your code goes here
+categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"]
+
+puts "Please, add a new categorie"
+extra_categorie = gets.chomp.capitalize
+
+categories << extra_categorie
+categories = categories.sort
+categories.each do |category|
+ puts "- #{category}"
+end
