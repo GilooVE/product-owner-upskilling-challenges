1624cb84273b1c134a5584a014b4576c84dcc614
Giloo <gve@lucca.fr>
2019-10-09 16:43:39 +0000

    exo ERB

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -5,9 +5,10 @@ require "sinatra/reloader" if development?
 enable :static
 
 get "/" do
-  @activities = [
-    # TODO: gather a few activities
+  @activities =
+  [{"name" => "Visit art museum", "category" => "Cultural", "city" => "Nantes","duration" => 120, "number of participants" => 5, "price (in EUR)" => 30},
+  {"name" => "Diner dans les arbres", "category" => "Food", "city" => "Nantes","duration" => 180, "number of participants" => 2, "price (in EUR)" => 100},
+  {"name" => "Escape Game", "category" => "Adventure", "city" => "Paris","duration" => 60, "number of participants" => 10, "price (in EUR)" => 50}
   ]
-
   erb :index
 end
