08b1f3bb59366a4c55ea7d30d52513a77b42cb1f
Giloo <gve@lucca.fr>
2019-10-10 16:48:36 +0000

    insert img

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -6,9 +6,9 @@ enable :static
 
 get "/" do
   @activities =
-  [{"name" => "Visit art museum", "category" => "Cultural", "city" => "Nantes","duration" => 120, "number of participants" => 5, "price (in EUR)" => 30},
-  {"name" => "Diner dans les arbres", "category" => "Food", "city" => "Nantes","duration" => 180, "number of participants" => 2, "price (in EUR)" => 100},
-  {"name" => "Escape Game", "category" => "Adventure", "city" => "Paris","duration" => 60, "number of participants" => 10, "price (in EUR)" => 50}
+  [{"name" => "Visit art museum", "category" => "Cultural", "city" => "Nantes","img url" => "\img\Musee.png", "duration" => 120, "number of participants" => 5, "price (in EUR)" => 30},
+  {"name" => "Diner dans les arbres", "category" => "Food", "city" => "Nantes","img" => "\img\arbre.png","duration" => 180, "number of participants" => 2, "price (in EUR)" => 100},
+  {"name" => "Escape Game", "category" => "Adventure", "city" => "Paris","img" => "\img\escape.jpg","duration" => 60, "number of participants" => 10, "price (in EUR)" => 50}
   ]
   erb :index
 end
