eff095645abd50058f2c1678797d2341b55f4864
Giloo <gve@lucca.fr>
2019-10-11 14:34:23 +0000

    exo 1 ruby programm

--- 05-api-consumption/01-ruby-program/search_user.rb
+++ 05-api-consumption/01-ruby-program/search_user.rb
@@ -2,8 +2,19 @@ require "json"
 require "rest-client"
 
 puts "What is the name of the user you're looking for? (e.g. Bob Dylan)"
-name = gets.chomp # asks user to type something in the Terminal
+lastName = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it! Let's search for that person..."
 
-# TODO: your code goes here
+
+url = "https://wagon.ilucca-demo.net/api/v3/users/"
+
+response = RestClient.get(url, "params" => {"fields" => "lastName, firstName, jobTitle, department","lastName" => "#{lastName},"}, "Authorization" => "Lucca application=18afcfb8-697b-4742-b726-418269485cee")
+users = JSON.parse(response.body)
+p users
+
+user = users["data"]["items"][0]
+puts "#{user["lastName"]}"
+puts "#{user["firstName"]}"
+puts "#{user["jobTitle"]}"
+puts "#{user["department"]["name"]}"
